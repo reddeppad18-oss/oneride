e1f225909138365f68ac76765c19271696ee0586
Reddeppa Diviti <reddeppa.d18@gmail.com>
2026-06-19 22:09:23 +0530

    Initial Commit

---
+++ src/test/java/one/oneride/OnerideApplicationTests.java
@@ -0,0 +1,13 @@
+package one.oneride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnerideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
