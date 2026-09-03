14e78ffd83d90ee89504ce8defed08e0d8e5fb98
Reddeppa Diviti <reddeppa.d18@gmail.com>
2026-09-04 00:00:40 +0530

    Resolve error

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://oneride-backend-obpl.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 
   headers: {
     "Content-Type": "application/json",
