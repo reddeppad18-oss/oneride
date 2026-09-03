63b3a4be5f3659158b92250e2ce2d66b833d5c2e
Reddeppa Diviti <reddeppa.d18@gmail.com>
2026-09-03 23:51:58 +0530

    Add frontend for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://oneride-backend-obpl.onrender.com",
 
   headers: {
     "Content-Type": "application/json",
