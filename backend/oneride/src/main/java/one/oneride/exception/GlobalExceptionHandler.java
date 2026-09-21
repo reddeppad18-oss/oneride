a4a3086c1c3f5ebd9a13b32a7a2d6afb59992a64
Reddeppa Diviti <reddeppa.d18@gmail.com>
2026-09-21 21:47:47 +0530

    Add exception logging

--- backend/oneride/src/main/java/one/oneride/exception/GlobalExceptionHandler.java
+++ backend/oneride/src/main/java/one/oneride/exception/GlobalExceptionHandler.java
@@ -1,22 +1,20 @@
 package one.oneride.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-
     private ResponseEntity<Map<String, Object>> buildResponse(
             HttpStatus status,
             String message) {
-
 
         Map<String, Object> response = new HashMap<>();
 
@@ -35,58 +33,45 @@ public class GlobalExceptionHandler {
                 message
         );
 
-
         return new ResponseEntity<>(
                 response,
                 status
         );
     }
 
-
-
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<?> handleUserNotFound(
             UserNotFoundException exception) {
-
 
         return buildResponse(
                 HttpStatus.NOT_FOUND,
                 exception.getMessage()
         );
     }
-
-
 
     @ExceptionHandler(RideNotFoundException.class)
     public ResponseEntity<?> handleRideNotFound(
             RideNotFoundException exception) {
 
-
         return buildResponse(
                 HttpStatus.NOT_FOUND,
                 exception.getMessage()
         );
     }
-
-
 
     @ExceptionHandler(BookingNotFoundException.class)
     public ResponseEntity<?> handleBookingNotFound(
             BookingNotFoundException exception) {
 
-
         return buildResponse(
                 HttpStatus.NOT_FOUND,
                 exception.getMessage()
         );
     }
 
-
-
     @ExceptionHandler(UnAuthorisedException.class)
     public ResponseEntity<?> handleUnauthorized(
             UnAuthorisedException exception) {
-
 
         return buildResponse(
                 HttpStatus.FORBIDDEN,
@@ -94,12 +79,9 @@ public class GlobalExceptionHandler {
         );
     }
 
-
-
     @ExceptionHandler(InvalidStateException.class)
     public ResponseEntity<?> handleInvalidState(
             InvalidStateException exception) {
-
 
         return buildResponse(
                 HttpStatus.BAD_REQUEST,
@@ -107,18 +89,19 @@ public class GlobalExceptionHandler {
         );
     }
 
-
-
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleGeneralException(
             Exception exception) {
 
+        // Print complete exception and stack trace to Render logs
+        exception.printStackTrace();
 
         return buildResponse(
                 HttpStatus.INTERNAL_SERVER_ERROR,
                 exception.getMessage()
         );
     }
+
     @ExceptionHandler(RentalNotFoundException.class)
     public ResponseEntity<?> handleRentalNotFound(
             RentalNotFoundException exception) {
